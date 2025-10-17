8225ca0b2c1916848bdbbed26f2f393fe5224c92
Maks <141412995+drbdedevil@users.noreply.github.com>
2025-10-17 20:12:00 +0500

    Init game data types

---
+++ GameDataManager/GameDataTypes.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+
+public class WeaponData
+{
+    public string ID { get; set; }
+    public string Name { get; set; }
+    public int Damage { get; set; }
+    public int AttackShapeID { get; set; }
+    public int EffectID { get; set; }
+    public string TextureName { get; set; }
+}
+
+public class CharacterData
+{
+    public string Name { get; set; }
+    public string Portrait { get; set; }
+    public int Damage { get; set; }
+    public int Heal { get; set; }
+    public int Speed { get; set; }
+    public bool[] Upgrades { get; set; }
+    public WeaponData Weapon { get; set; }
+}
+
+public class StorageData
+{
+    public int Level { get; set; }
+    public int Crystals { get; set; }
+    public int ChitinFragments { get; set; }
+    public List<WeaponData> Weapons { get; set; }
+}
+
+public class LivingSpaceData
+{
+    public int Level { get; set; }
+    public List<CharacterData> UsedCharacters { get; set; }
+    public List<CharacterData> ReserveCharacters { get; set; }
+}
+
+public class ForgeData
+{
+    public int Level { get; set; }
+}
+
+public class TrainingPitsData
+{
+    public int Level { get; set; }
+    public List<CharacterData> CharactersForHiring { get; set; }
+}
+
+public class CommandBlockData
+{
+    public int RaidCount { get; set; }
+    public int EnemyDefeated { get; set; }
+    public int SquadLevel { get; set; }
+    public bool VegetableDefeated { get; set; }
+    public bool TankDefeated { get; set; }
+    public bool SpiderBossDefeated { get; set; }
+}
+
+public class RunMapData
+{
+    
+}
+
+public class GameData
+{
+    public StorageData storageData = new();
+    public LivingSpaceData livingSpaceData = new();
+    public ForgeData forgeData = new();
+    public TrainingPits trainingPits = new();
+    public CommandBlockData commandBlockData = new();
+    public RunMapData runMapData = new();
+}
