85db74f4d9a99d5e89d1ca8fe0ef74db428c1ef5
Maks <141412995+drbdedevil@users.noreply.github.com>
2025-11-13 02:25:43 +0500

    Enemy weapon viewing

--- Levels/HubLocation/WarriorPanels/ViewEnemyWarriorPanel.cs
+++ Levels/HubLocation/WarriorPanels/ViewEnemyWarriorPanel.cs
@@ -1,6 +1,8 @@
 using Godot;
 using RaidIntoTheDeep.Levels.Fight;
+using RaidIntoTheDeep.Levels.Fight.Weapons;
 using System;
+using System.Linq;
 
 public partial class ViewEnemyWarriorPanel : Control
 {
@@ -12,7 +14,7 @@ public partial class ViewEnemyWarriorPanel : Control
 	public override void _Ready()
 	{
 		MouseEntered += OnMouseEnter;
-        MouseExited += OnMouseExit;
+		MouseExited += OnMouseExit;
 	}
 
 	public void SetEnemyInfosToWarriorEnemyPanel(EnemyEntity enemyEntity)
@@ -25,19 +27,38 @@ public partial class ViewEnemyWarriorPanel : Control
 	{
 		WarriorEnemy warrior = GetNode<WarriorEnemy>("PanelContainer/MarginContainer/WarriorEnemyPanel");
 		return warrior.EnemyEntity;
-    }
+	}
 
 	private void OnMouseEnter()
 	{
 		WarriorEnemy warrior = GetNode<WarriorEnemy>("PanelContainer/MarginContainer/WarriorEnemyPanel");
 
 		EmitSignal(SignalName.EnemyPanelMouseEnter, warrior.EnemyEntity);
-    }
 
-    private void OnMouseExit()
+		MarginContainer marginContainer = GetNode<MarginContainer>("PanelContainer/WeaponINfs");
+		marginContainer.Visible = true;
+
+		EffectInfo effectInfo = GameDataManager.Instance.effectDatabase.Effects.FirstOrDefault(effect => effect.effectType == warrior.EnemyEntity.Weapon.effect.EffectType);
+		if (effectInfo != null)
+		{
+			TextureRect effectRect = GetNode<TextureRect>("PanelContainer/WeaponINfs/ColorRect/TextureRect2/MarginContainer/EffectTexture");
+			effectRect.Texture = effectInfo.texture2D;
+		}
+		WeaponRow weaponRow = GameDataManager.Instance.weaponDatabase.Weapons.FirstOrDefault(weapon => weapon.Name == warrior.EnemyEntity.Weapon.weaponData.Name);
+		if (weaponRow != null)
+		{
+			TextureRect weaponRect = GetNode<TextureRect>("PanelContainer/WeaponINfs/ColorRect/TextureRect");
+			weaponRect.Texture = weaponRow.WeaponTexture;
+		}
+	}
+
+	private void OnMouseExit()
 	{
 		WarriorEnemy warrior = GetNode<WarriorEnemy>("PanelContainer/MarginContainer/WarriorEnemyPanel");
 
 		EmitSignal(SignalName.EnemyPanelMouseExit, warrior.EnemyEntity);
-    }
+
+		MarginContainer marginContainer = GetNode<MarginContainer>("PanelContainer/WeaponINfs");
+		marginContainer.Visible = false;
+	}
 }
