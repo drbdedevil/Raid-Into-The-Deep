aa588a1314dee2348d339771d886fa01e0527caa
Klimov Artem <artem_klimov_2002@bk.ru>
2025-11-15 21:41:17 +0500

    MapWeights

--- GameDataManager/GameData/RaceMap/RaceMapValuesDatabase.cs
+++ GameDataManager/GameData/RaceMap/RaceMapValuesDatabase.cs
@@ -4,10 +4,10 @@ using System;
 [GlobalClass]
 public partial class RaceMapValuesDatabase : Resource
 {
-    [Export] public int MinCrystalsAward = 10;
-    [Export] public int MaxCrystalsAward = 50;
-    [Export] public int MinChitinFragmentsAward = 10;
-    [Export] public int MaxChitinFragmentsAward = 50;
+	[Export] public int MinCrystalsAward = 10;
+	[Export] public int MaxCrystalsAward = 50;
+	[Export] public int MinChitinFragmentsAward = 10;
+	[Export] public int MaxChitinFragmentsAward = 50;
 
-    [Export] public int HealPercent = 25;
+	[Export] public int HealPercent = 25;
 }
